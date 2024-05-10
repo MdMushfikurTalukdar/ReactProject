2dcc8fdd55c297737befb1242d93b700211654d0
Md Mushfikur Talukdar <mithutalukdar09@gmail.com>
2024-05-10 23:07:29 +0600

    navbar uploaded and home page design is modifying..

--- education/src/App.js
+++ education/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
 import { Route, Routes } from "react-router-dom";
+import Navbar from "./Components/Pages/Navbar";
 import Home from "./Components/Pages/Home";
 
 function App() {
   return (
     <>
+      <Navbar platform="Education" home="Home" aboutUs="About Us" login="Login" />
       <Home />
     </>
   );
