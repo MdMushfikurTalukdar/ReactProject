25c30470b2895ac10ec670b8c35f718266cad1ba
Someswar Gorai <somgorai726@gmail.com>
2024-05-11 11:52:49 +0530

    register page

--- education/src/App.js
+++ education/src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
-
-import { Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Pages/Navbar";
 import Home from "./Components/Pages/Home";
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import { RegisterUser } from "./pages/RegisterUser";
 
 function App() {
   return (
     <>
-      <Navbar platform="Education" home="Home" aboutUs="About Us" login="Login" />
-      <Home />
+    <Navbar platform="Education" home="Home" aboutUs="About Us" login="Login" />
+    <Router>
+      <Routes>
+        <Route path="/register" element={<RegisterUser />} />
+        <Route path="/Home" element={<Home />} />
+      </Routes>
+    </Router>
     </>
   );
 }
