875c21495ce5177c91eb892c29f570baaf4346a9
Siddharth Maddheshiya <bhaisiddharth63@gmail.com>
2024-05-16 09:23:33 -0700

    certificte frontend and dyanic code done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-import React from 'react'
-// import RegistrationPage from './components/Registration'
-import { BrowserRouter as Router ,Routes, Route} from 'react-router-dom'
-import {LoginPage} from "./Pages/Login.jsx"
-// import "App.css"
+import Certificate from './components/Certificate'
 function App() {
+
   return (
-  
-    <Router>
-    <Routes>
-      <Route path="/login" element={<LoginPage/>} />
-    </Routes>
-    </Router>
-  )
+
+    <Certificate/>
+
+  );
 }
 
-export default App
+export default App;
