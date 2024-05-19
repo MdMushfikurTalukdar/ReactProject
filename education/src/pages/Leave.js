8f1ee5db0f462cf5a3abcf2377befc37e17a61a5
Md Mushfikur Talukdar <mithutalukdar09@gmail.com>
2024-05-19 13:56:57 +0600

    .

--- education/src/pages/Leave.js
+++ education/src/pages/Leave.js
@@ -47,7 +47,7 @@ export const Leave = () => {
                 <p>Transfer / Leaving Certificate</p>
               </div>
               <div className='textDate'>
-                <span className='sl'>Sl. No.</span><span className='date'>Date........................</span>
+                <span className='sl'>Sl. No.</span><span className='date'>Date : <input className='dateIn' required placeholder='20/3/2024'/></span>
               </div>
               <div className='allText'>
                 <p>1. Pupil's Name</p>
@@ -60,6 +60,25 @@ export const Leave = () => {
                 <p>8. Class in wich he was then reading-</p>
                 <p>9. (To be filled up when the pupil leaves at the end of the session) whether he has passed the examination for promotion to the next higher class- </p>
                 <p>10 (To be filled up when the pupil leaves during the session) attendance during the current years-</p>
+              </div>
+              <div className='allTextFields'>
+                <input className='name' placeholder='Pupil Name' required />
+                <input className='father' placeholder='Father Name-' required />
+                <input className='residence' placeholder='Residence-' required />
+                <input className='admission' placeholder='Date of admission-' required />
+                <input className='register' placeholder='Number of in adm. Register-' required />
+                <input className='birth' placeholder='Date of Birth as recorded in the Adm. Reg.-' required />
+                <input className='institute' placeholder='Date on wich he left the institute-' required />
+                <input className='reading' placeholder='Class in wich he was then reading-' required />
+                <input className='higher' placeholder='whether he has passed the examination for promotion to the next higher class- ' />
+                <input className='current' placeholder='attendance during the current years-' required />
+                <input className='dofadd' placeholder='Date of admission' required />
+                <input className='dofleave' placeholder='Date of Leaving' required />
+                <input className='Character' placeholder='Character-' required />
+                <input className='reason' placeholder='Reason for leaving-' required />
+                <input className='issue' placeholder='Issuing clerk-' required />
+                <input className='dissue' placeholder='Date of Issue-' required />
+                <input className='sign' placeholder='signature' required />
               </div>
               <div className='allTextTable'>
                 <table>
