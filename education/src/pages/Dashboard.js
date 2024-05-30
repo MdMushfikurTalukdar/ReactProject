3d8d6b70500ae6b71496616dd101d18e547331f1
Md Mushfikur Talukdar <mithutalukdar09@gmail.com>
2024-05-30 16:51:57 +0600

    updated

--- education/src/pages/Dashboard.js
+++ education/src/pages/Dashboard.js
@@ -14,48 +14,48 @@ import NavbarNew from "../components/NavbarNew";
 export const Dashboard = () => {
   const navigate = useNavigate();
 
-  function regenerateToken(){
-    let data = JSON.stringify({
-      "refresh": localStorage.getItem('refreshtoken')
-    });
+  // function regenerateToken(){
+  //   let data = JSON.stringify({
+  //     "refresh": localStorage.getItem('refreshtoken')
+  //   });
     
-    let config = {
-      method: 'post',
-      maxBodyLength: Infinity,
-      url: 'https://amarnath013.pythonanywhere.com/api/user/token/refresh/',
-      headers: { 
-        'Content-Type': 'application/json'
-      },
-      data : data
-    };
+  //   let config = {
+  //     method: 'post',
+  //     maxBodyLength: Infinity,
+  //     url: 'https://amarnath013.pythonanywhere.com/api/user/token/refresh/',
+  //     headers: { 
+  //       'Content-Type': 'application/json'
+  //     },
+  //     data : data
+  //   };
     
-    axios.request(config)
-    .then((response) => {
-      console.log(JSON.stringify(response.data));
-      localStorage.setItem('refreshtoken', response.data.refresh);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  //   axios.request(config)
+  //   .then((response) => {
+  //     console.log(JSON.stringify(response.data));
+  //     localStorage.setItem('refreshtoken', response.data.refresh);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
     
-  }
+  // }
 
 
-  useEffect(() => {
-    if (localStorage?.getItem("accesstoken")) {
-      const response = jwtDecode(localStorage?.getItem("accesstoken"));
-      if (
-        response.token_type !== "access" &&
-        typeof response.user_id !== Number &&
-        typeof response.jti !== String &&
-        response.exp < Math.floor(Date.now() / 1000)
-      ) {
-        regenerateToken();
-      }
-    } else {
-      navigate("/login");
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (localStorage?.getItem("accesstoken")) {
+  //     const response = jwtDecode(localStorage?.getItem("accesstoken"));
+  //     if (
+  //       response.token_type !== "access" &&
+  //       typeof response.user_id !== Number &&
+  //       typeof response.jti !== String &&
+  //       response.exp < Math.floor(Date.now() / 1000)
+  //     ) {
+  //       regenerateToken();
+  //     }
+  //   } else {
+  //     navigate("/login");
+  //   }
+  // }, []);
 
   return (
     <Box style={{fontFamily:"cursive"}}>
