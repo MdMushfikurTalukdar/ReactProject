d73c25f86b20323f48c05d589d4dc5e8f10a1c71
Someswar Gorai <somgorai726@gmail.com>
2024-06-28 17:32:06 +0530

    updated

--- education/src/components/NavbarNew.js
+++ education/src/components/NavbarNew.js
@@ -206,7 +206,7 @@ export const NavbarNew = () => {
                 <div className="absolute left-0 mt-2 w-64 bg-white text-black rounded-lg shadow-lg z-50">
                   <div className="py-2">
                     <div
-                      className=" px-4 py-2 cursor-pointer"
+                      className="hover:bg-blue-50 px-4 py-2 cursor-pointer"
                       onClick={toggleNestedDropdownGeneral}
                     >
                       {mainOptions[0]}
@@ -231,7 +231,7 @@ export const NavbarNew = () => {
                     {roll === "student" && (
                       <>
                         <div
-                          className=" px-4 py-2 cursor-pointer"
+                          className="hover:bg-blue-50 px-4 py-2 cursor-pointer"
                           onClick={toggleNestedDropdownAcademic}
                         >
                           {mainOptions[1]}
