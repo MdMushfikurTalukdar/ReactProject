01b4cfe4728ad8d6cdb57e9de6cb76311a7f2b41
Someswar Gorai <somgorai726@gmail.com>
2024-06-28 18:55:45 +0530

    updated

--- education/src/pages/Login.js
+++ education/src/pages/Login.js
@@ -8,7 +8,8 @@ import Footer from '../components/Home/Footer';
 import Cookies from 'js-cookie';
 import "./Login.css"
 import {
-  Button
+  Button,
+  TextField
 } from "@mui/material";
 import Style from '../components/StyleLogin';
 
@@ -69,31 +70,36 @@ export const LoginPage = () => {
     <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 loginBody">
       <div className="max-w-md w-full space-y-8">
         <div>
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900 textSign">Sign in to your account</h2>
+          <p className="mt-6 text-center text-3xl text-gray-900 textSign">Sign in to your account</p>
         </div>
         <form className="mt-8 space-y-6" onSubmit={handleSubmit(onSubmit)}>
           <input type="hidden" name="remember" defaultValue="true" />
           <div className="rounded-md shadow-sm -space-y-px">
             <div>
               <label htmlFor="roll-number" className="sr-only">Registration no</label>
-              <input
+              <TextField
                 id="roll-number"
                 type="text"
+                label="Registration no./Employee ID."
+                fullWidth
                 {...register("rollNumber", { required: true })}
                 autoComplete="off"
                 className={`appearance-none rounded-none relative block w-full px-3 py-2 border ${errors.rollNumber ? 'border-red-500' : 'border-gray-300'} placeholder-blue-300 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm`}
                 placeholder="Registration no./Employee ID."
+               
               />
               {errors.rollNumber && <p className="text-black-500 text-xs mt-1 textSign">Roll Number is required</p>}
             </div>
             <div>
               <label htmlFor="password" className="sr-only">Password</label>
-              <input
+              <TextField
                 id="password"
                 type="password"
+                label="password"
                 {...register("password", { required: true })}
+                fullWidth
                 autoComplete="current-password"
-                className={`appearance-none rounded-none mt-4 relative block w-full px-3 py-2 border ${errors.password ? 'border-red-500' : 'border-gray-300'} placeholder-blue-300 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm`}
+                sx={{marginTop:"10px"}}
                 placeholder="Password"
               />
               {errors.password && <p className="text-black-500 text-xs mt-1 textSign">Password is required</p>}
