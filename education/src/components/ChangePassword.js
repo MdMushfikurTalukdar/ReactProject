98a77f4bbb52ef6ba2043337e869949a4ef5001d
Someswar Gorai <somgorai726@gmail.com>
2024-06-28 18:47:50 +0530

    updated

--- education/src/components/ChangePassword.js
+++ education/src/components/ChangePassword.js
@@ -34,30 +34,30 @@ export const ChangePassword = () => {
     resolver: yupResolver(validationSchema),
   });
 
-  function regenerateToken() {
-    let data = JSON.stringify({
-      "refresh": localStorage.getItem('refreshtoken')
-    });
+  // function regenerateToken() {
+  //   let data = JSON.stringify({
+  //     "refresh": localStorage.getItem('refreshtoken')
+  //   });
 
-    let config = {
-      method: 'post',
-      maxBodyLength: Infinity,
-      url: 'https://amarnath013.pythonanywhere.com/api/user/token/refresh/',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      data: data
-    };
+  //   let config = {
+  //     method: 'post',
+  //     maxBodyLength: Infinity,
+  //     url: 'https://amarnath013.pythonanywhere.com/api/user/token/refresh/',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     data: data
+  //   };
 
-    axios.request(config)
-      .then((response) => {
-        console.log(JSON.stringify(response.data));
-        localStorage.setItem('refreshtoken', response.data.refresh);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
+  //   axios.request(config)
+  //     .then((response) => {
+  //       console.log(JSON.stringify(response.data));
+  //       localStorage.setItem('refreshtoken', response.data.refresh);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // }
 
   useEffect(() => {
     if (localStorage?.getItem("accesstoken")) {
@@ -68,7 +68,7 @@ export const ChangePassword = () => {
         typeof response.jti !== String &&
         response.exp < Math.floor(Date.now() / 1000)
       ) {
-        regenerateToken();
+        navigate("/login");
       }
     } else {
       navigate("/login");
