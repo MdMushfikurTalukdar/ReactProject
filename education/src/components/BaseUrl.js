67b9b2e6133ad71e62b81b93c43e27c58f09d74f
Someswar Gorai <somgorai726@gmail.com>
2024-09-08 11:48:09 +0530

    updated

--- education/src/components/BaseUrl.js
+++ education/src/components/BaseUrl.js
@@ -1,3 +1,3 @@
-export const BaseUrl="http://13.233.119.2"
-export const Url='http://13.233.119.2'
+export const BaseUrl="http://13.233.119.2/api/user"
+export const Url='http://13.233.119.2/api/user'
 
